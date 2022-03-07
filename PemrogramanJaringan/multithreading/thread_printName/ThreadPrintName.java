59ddb70a8fecef64b0d5a5a38cb7f62fa3f17ea1
Nabilatul Arofah <54842644+nabilatularofah@users.noreply.github.com>
2022-03-08 01:47:28 +0700

    Add files via upload

---
+++ PemrogramanJaringan/multithreading/thread_printName/ThreadPrintName.java
@@ -0,0 +1,27 @@
+package thread_printName;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ThreadPrintName extends Thread {
+    public ThreadPrintName(String name) {
+        super(name);
+        start();//menjalankan thread dengan satu kali instansiasi
+    }
+
+    @Override
+    public void run() {
+        String name = getName();
+        for(int i=0; i<20; i++){
+            System.out.print(name);
+            try{
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException ex){
+                Logger.getLogger(ThreadPrintName.class.getName()).log(Level.SEVERE, null, ex);
+                //ex.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ PemrogramanJaringan/multithreading/thread_printName/main.java
@@ -0,0 +1,10 @@
+package thread_printName;
+
+public class main {
+    public static void main(String[] args){
+        ThreadPrintName pnt1 = new ThreadPrintName ("A");
+        ThreadPrintName pnt2 = new ThreadPrintName ("B");
+        ThreadPrintName pnt3 = new ThreadPrintName ("C");
+        ThreadPrintName pnt4 = new ThreadPrintName ("D");
+    }
+}
