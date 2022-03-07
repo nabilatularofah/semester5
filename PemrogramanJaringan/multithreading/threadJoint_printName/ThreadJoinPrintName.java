143945f3b6c65361633537f56c810aeb7bf76fa3
Nabilatul Arofah <54842644+nabilatularofah@users.noreply.github.com>
2022-03-08 01:50:52 +0700

    Add files via upload

---
+++ PemrogramanJaringan/multithreading/threadJoint_printName/ThreadJoinPrintName.java
@@ -0,0 +1,26 @@
+package threadJoint_printName;
+
+public class ThreadJoinPrintName implements Runnable {
+    Thread thread;
+
+    public ThreadJoinPrintName(String name){
+        thread = new Thread(this, name);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        String name = thread.getName();
+        for(int i=0; i<20; i++){
+            System.out.println(name);
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        
+    }
+
+}
---
+++ PemrogramanJaringan/multithreading/threadJoint_printName/main.java
@@ -0,0 +1,23 @@
+package threadJoint_printName;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Threads will be running");
+
+        ThreadJoinPrintName pnt1 = new ThreadJoinPrintName("A");
+        ThreadJoinPrintName pnt2 = new ThreadJoinPrintName("B");
+        ThreadJoinPrintName pnt3 = new ThreadJoinPrintName("C");
+        ThreadJoinPrintName pnt4 = new ThreadJoinPrintName("D");
+
+        try{
+            pnt1.thread.join();
+            pnt2.thread.join();
+            pnt3.thread.join();
+            pnt4.thread.join();
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Threads killed.");
+    }
+}
