62a88de8b36e857faf1aa12f812478e3f4774ff6
Nabilatul Arofah <54842644+nabilatularofah@users.noreply.github.com>
2022-03-08 01:48:30 +0700

    Add files via upload

---
+++ PemrogramanJaringan/multithreading/thread_printName2/main.java
@@ -0,0 +1,10 @@
+package thread_printName2;
+
+public class main {
+    public static void main(String[] args) {
+        /*PrintName Thread pnt1=*/ new ThreadPrintName2("A");
+        /*PrintName Thread pnt12=*/ new ThreadPrintName2("B");
+        /*PrintName Thread pnt3=*/ new ThreadPrintName2("C");
+        /*PrintName Thread pnt4=*/ new ThreadPrintName2("D");
+    }
+}
